24e27bdef0db869e505c3d7a661ef236b36587e2
snh1003 <lunasaprismriver57@gmail.com>
2020-10-26 23:15:41 +0900

    fix uploader

--- app/API/V1/fileupload.ts
+++ app/API/V1/fileupload.ts
@@ -4,11 +4,12 @@ import moment from "moment"
 
 const storage = multer.diskStorage({
     destination: function(req, file:Express.Multer.File, cb: (error: Error | null, destination: string) => void) {
-        cb(null, 'uploads');
+        cb(null, './uploads');
     },
     filename: function(req, file:Express.Multer.File, cb: (error: Error | null, filename: string) => void) {
         cb(null, moment().format('YYYYMMDDHHmmss') + "_" + file.originalname);
+        // cb(null , file.originalname);
     }
 });
 
-export const upload = multer({ storage: storage }).single("file");
+export const upload = multer({ storage: storage })
